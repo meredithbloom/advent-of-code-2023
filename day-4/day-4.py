c79acff65f652012587aca5d0361960b0d348619
Meredith Bloom <meredithbloom@merediths-mbp.lan>
2023-12-11 20:30:15 -0500

    day 4 part 1 solved

--- day-4/day-4.py
+++ day-4/day-4.py
@@ -4,16 +4,36 @@ test_all_cards = open('test.txt', 'r')
 cards = all_cards.read().split('\n')
 test_cards = test_all_cards.read().split('\n')
 
-print(test_cards[0])
+#print(test_cards[0])
 
 def parse_card(card_string):
     card_num, only_nums = card_string.split(':')[0].strip(), card_string.split(':')[1].strip()
     winners, have = only_nums.split('|')
-    print(winners)
-    return winners, have
+    #print(only_nums)
+    return only_nums
 
 
-def compare_nums(winners, ):
-    continue
+def compare_nums(nums):
+    left, right = nums.split('|')[0].strip(), nums.split('|')[1].strip()
+    left_nums = {int(x) for x in left.split()}
+    right_nums = {int(x) for x in right.split()}
+    overlap = left_nums.intersection(right_nums)
+    if (len(overlap) > 0):
+        return(calc_points(overlap))
+    else:
+        return 0
 
-parse_card(test_cards[0])
+def calc_points(num_intersection):
+    if (len(num_intersection) == 1):
+        return 2**0
+    elif (len(num_intersection) > 1):
+        return 2**(len(num_intersection)-1)
+    
+
+total = 0
+for c in cards:
+    parsed = parse_card(c)
+    to_add = compare_nums(parsed)
+    total = total + to_add
+
+print(total)
